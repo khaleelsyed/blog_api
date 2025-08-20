e6eb61988068889c73507a79e7f323c1e5be1316
khaleelsyed <khaleelsyed@users.noreply.github.com>
2025-08-21 00:40:11 +0100

    boilerplate endpoints

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,15 +1,61 @@
 package api
 
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type apiHandlerFunc func(w http.ResponseWriter, r *http.Request) error
+
+var methodNotAllowed = func(w http.ResponseWriter) error {
+	return WriteJSON(w, http.StatusMethodNotAllowed, errors.New("method not allowed"))
+}
+
 type APIServer struct {
 	listenAddr string
 	storage    Storage
 }
 
-func (s *APIServer) Run() {}
+func (a APIServer) Run() {
+	r := mux.NewRouter()
+	r.HandleFunc("/posts", makeHTTPHandler(a.handlePostsEndpoint))
 
-func NewAPIServer(listenAddr string, storage Storage) *APIServer {
-	return &APIServer{
+	if err := http.ListenAndServe(a.listenAddr, r); err != nil {
+
+	}
+}
+
+func NewAPIServer(listenAddr string, storage Storage) APIServer {
+	return APIServer{
 		listenAddr: listenAddr,
 		storage:    storage,
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	if _, errFound := v.(error); errFound && status < 400 {
+		status = http.StatusInternalServerError
+	}
+
+	w.WriteHeader(status)
+
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		log.Println(err)
+	}
+	return nil
+}
+
+func makeHTTPHandler(f apiHandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusInternalServerError, err)
+		}
 	}
 }
--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,1 +1,22 @@
 package api
+
+import "net/http"
+
+func (a APIServer) handlePostsEndpoint(w http.ResponseWriter, r *http.Request) error {
+	switch r.Method {
+	case http.MethodPost:
+		return a.handleCreatePost(w, r)
+	case http.MethodGet:
+		return a.handleListPosts(w, r)
+	default:
+		return methodNotAllowed(w)
+	}
+}
+
+func (a APIServer) handleCreatePost(w http.ResponseWriter, r *http.Request) error {
+	return WriteJSON(w, http.StatusNotImplemented, "create post not implemented")
+}
+
+func (a APIServer) handleListPosts(w http.ResponseWriter, r *http.Request) error {
+	return WriteJSON(w, http.StatusNotImplemented, "list posts not implemented")
+}
