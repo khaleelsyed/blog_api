d6a86051cca5b38842ea86cc060767395df3c93e
khaleelsyed <khaleelsyed@users.noreply.github.com>
2025-08-21 23:48:03 +0100

    refactor

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -9,17 +9,6 @@ import (
 	"github.com/khaleelsyed/blog_api/internal/storage"
 )
 
-func (a APIServer) handlePostsEndpoint(w http.ResponseWriter, r *http.Request) error {
-	switch r.Method {
-	case http.MethodPost:
-		return a.handleCreatePost(w, r)
-	case http.MethodGet:
-		return a.handleListPosts(w, r)
-	default:
-		return methodNotAllowed(w)
-	}
-}
-
 func (a APIServer) handleCreatePost(w http.ResponseWriter, r *http.Request) error {
 	var postCreate storage.PostContent
 
@@ -45,4 +34,15 @@ func (a APIServer) handleListPosts(w http.ResponseWriter, r *http.Request) error
 	}
 
 	return WriteJSON(w, http.StatusOK, posts)
+}
+
+func (a APIServer) handlePostsEndpoint(w http.ResponseWriter, r *http.Request) error {
+	switch r.Method {
+	case http.MethodPost:
+		return a.handleCreatePost(w, r)
+	case http.MethodGet:
+		return a.handleListPosts(w, r)
+	default:
+		return methodNotAllowed(w)
+	}
 }
